c958884aed325fc78acfd6182f3bbd5fb2c0744e
Alesbadmeng <xe889140@163.com>
2022-12-08 14:59:33 +0800

    画蛇+逻辑表示+画图

--- web/src/assets/scripts/GameMap.js
+++ web/src/assets/scripts/GameMap.js
@@ -1,8 +1,9 @@
 import { GameObject } from "./GameObject"; //export就用{} 如果是export default就不用{}
+import { Snake } from "./Snake";
 import { Wall } from "./Wall";
 
 export class GameMap extends GameObject{
-        constructor(ctx, parent) {
+        constructor(ctx, parent) { //相当于gamemap的结构体
             super();
     
             this.ctx = ctx;
@@ -10,10 +11,17 @@ export class GameMap extends GameObject{
             this.L = 0;
 
             this.rows = 13;
-            this.cols = 13;
+            this.cols = 14;
             
-            this.inner_walls_count = 30;
+            this.inner_walls_count = 20;
             this.walls = [];
+
+            this.snakes = [
+                new Snake({id: 0,color:"#4876EC", r: this.rows-2 , c: 1},this),
+
+                new Snake({id: 1,color:"#F94848", r: 1, c: this.cols-2},this),
+            ];
+
     }
     //flooyd 检查连通性的算法
     check_connectivity(g,sx,sy,tx,ty){
@@ -22,7 +30,7 @@ export class GameMap extends GameObject{
 
          let dx = [-1,0,1,0],dy = [0,1,0,-1];
          for(let i= 0;i<4;i++){
-            let x = sx + dx[i],y = sy+dy[i];
+            let x = sx + dx[i],y = sy + dy[i];
             if(!g[x][y]&&this.check_connectivity(g,x,y,tx,ty))
                 return true;
          }
@@ -36,7 +44,7 @@ export class GameMap extends GameObject{
             for(let c = 0;c<this.cols;c++){
                 g[r][c] = false;//初始化
             }
-        }
+        }   
         //给四周加上障碍物
         //给左右加上墙
         for(let r = 0;r<this.rows;r++){
@@ -49,15 +57,15 @@ export class GameMap extends GameObject{
 
         //创建随机障碍物
         for(let i =0;i<this.inner_walls_count/2;i++){
-            for(let j =0;j<1000;j++){
+            for(let j =0;j<10000;j++){
                 let r = parseInt(Math.random()*this.rows);//random产生[0,1)的随机值,就是随机取一个r
                 let c = parseInt(Math.random()*this.cols);
-                if(g[r][c]||g[c][r]) continue;
+                if(g[r][c]||g[this.rows-r-1][this.cols-c-1]) continue;
                 //左下角格与右上角格不能有墙
                 if(r == this.rows-2&&c == 1 ||r ==1&& c==this.cols-2)
                 continue;
 
-                g[r][c] = g[r][c] = true;
+                g[r][c] = g[this.rows-r-1][this.cols-c-1] = true;
                 break;
             }
         }
@@ -78,11 +86,30 @@ export class GameMap extends GameObject{
         return true;
     }  
 
+    add_listening_events(){
+        this.ctx.canvas.focus();//要使用canvas的聚焦能力 需要设置设置 tabindex 属性
+
+        const [snake0,snake1] = this.snakes;    
+        this.ctx.canvas.addEventListener("keydown",e => {
+            if(e.key==="w") snake0.set_direction (0);
+            else if(e.key === "d") snake0.set_direction (1);
+            else if(e.key ===  "s") snake0.set_direction  (2);
+            else if(e.key === "a") snake0.set_direction (3);
+            else if(e.key === "ArrowUp") snake1.set_direction(0);
+            else if(e.key ==="ArrowRight") snake1.set_direction(1);
+            else if(e.key ==="ArrowDown") snake1.set_direction(2);
+            else if(e.key ==="ArrowLeft") snake1.set_direction(3);
+        });//从键盘获取输入，输出
+
+    }
+
     start(){
         for(let i =0;i<10000;i++){//循环10000次
             if(this.create_walls());//判断是否创建 create_walls函数返回了true和flase
             break;
         }
+
+        this.add_listening_events();
     }
 
     update_size(){
@@ -91,9 +118,45 @@ export class GameMap extends GameObject{
         this.ctx.canvas.height = this.L * this.rows;
     }
 
+    check_ready(){ //判断俩条蛇是否已经准备好进入下一步
+        for(const sanke of this.snakes){
+            if(sanke.status!=="idle") return false;
+            if(sanke.direction === -1 ) return false;   
+        }
+        return true;
+    }
+
+    next_step(){ //让两条蛇进入下一步   
+        for(const snake of this.snakes){
+            snake.next_step();
+        }
+    }
+
+    check_valid(cell){
+        for(const wall of this.walls){
+            if(wall.r === cell.r&&wall.c ===cell.c)
+            return false;
+        }
+
+        for(const snake of this.snakes){
+            let k = snake.cells.length;
+            if(!snake.check_tail_increasing()){
+                k--;
+            }
+            for(let i=0;i<k;i++){
+                if(snake.cells[i].r === cell.r&& snake.cells[i].c ===cell.c)
+                return false;
+            }
+        }
+        return true;
+    }
+
     update(){
         this.update_size();
-        this.render();
+        if(this.check_ready()){
+            this.next_step();
+        }
+        this.render();//渲染
     }
 
     render() {
